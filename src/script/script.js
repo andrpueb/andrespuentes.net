d8c13f92ad40d53d8067738e5492fb387cb8bcf3
Andres Puentes <andresfpuentes@gmail.com>
2017-05-30 20:46:15 +0100

    Live website first

--- src/script/script.js
+++ src/script/script.js
@@ -48,6 +48,40 @@ $(document).ready(function() {
   })
 
 
+  var $contactForm = $('#contact-form');
+  $contactForm.submit(function(e) {
+  	e.preventDefault();
+  	$.ajax({
+  		url: '//formspree.io/me@andrespuentes.net',
+  		method: 'POST',
+  		data: $(this).serialize(),
+  		dataType: 'json',
+  		beforeSend: function() {
+  			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+  		},
+  		success: function(data) {
+  			$contactForm.find('.alert--loading').hide();
+  			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+  		},
+  		error: function(err) {
+  			$contactForm.find('.alert--loading').hide();
+  			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+  		}
+  	});
+  });
 
+
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 
 });
